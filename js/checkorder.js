270ed8f7f0ae9a4b026cfa6912f487c6d0013d2f
dsmtan <denisetan@live.nl>
2019-04-03 16:32:09 +0200

    added shipping address

--- js/checkorder.js
+++ js/checkorder.js
@@ -7,6 +7,7 @@
 const pickupInput = document.querySelector("#freepickup");
 const shipInput = document.querySelector("#shiphome");
 const shipSum = document.querySelector("#shippingprice");
+const addressInfo = document.querySelector("#shippinginfo");
 
 pickupInput.addEventListener("click", checkShipping);
 shipInput.addEventListener("click", checkShipping);
@@ -14,7 +15,9 @@ shipInput.addEventListener("click", checkShipping);
 function checkShipping() {
   if (shipInput.checked) {
     shipSum.textContent = "50 DKK";
+    addressInfo.classList.remove("hide");
   } else {
     shipSum.textContent = "0 DKK";
+    addressInfo.classList.add("hide");
   }
 }
